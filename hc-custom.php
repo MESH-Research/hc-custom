157213258dbea1e72f3b255bd9ff612dc8fca38f
Ryan Williams <rwilliams@mla.org>
2017-10-04 11:19:56 -0400

    first

---
+++ hc-custom.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     HC Custom
+ * Plugin URI:      https://github.com/mlaa/hc-custom
+ * Description:     Miscellaneous actions & filters for Humanities Commons.
+ * Author:          MLA
+ * Author URI:      https://github.com/mlaa
+ * Text Domain:     hc-custom
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hc_Custom
+ */
