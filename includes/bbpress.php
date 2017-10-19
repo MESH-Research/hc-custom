f9694a0b34b86e9fdfd015987784cdc3864c19e4
Ryan Williams <rwilliams@mla.org>
2017-10-19 15:35:55 -0400

    preserve topic_data when filtering society group admin ham

--- includes/bbpress.php
+++ includes/bbpress.php
@@ -8,7 +8,7 @@
 /**
  * Ensure forum posts by group admins who are society members are never marked as spam.
  */
-function hcommons_allow_society_group_admins_ham() {
+function hcommons_allow_society_group_admins_ham( $topic_data ) {
 	$user_id = get_current_user_id();
 	$group_ids = bbp_get_forum_group_ids( $_REQUEST['bbp_forum_id'] );
 	$current_user_is_society_member = ( 1 < count( Humanities_Commons::hcommons_get_user_memberships() ) );
@@ -20,9 +20,11 @@ function hcommons_allow_society_group_admins_ham() {
 			break; // Trust user to crosspost if they're an admin of any group being posted to.
 		}
 	}
+
+	return $topic_data;
 }
 // Priority 0 to run before the actions that might be removed by this function.
-add_action( 'bbp_new_topic_pre_insert', 'hcommons_allow_society_group_admins_ham', 0 );
+add_filter( 'bbp_new_topic_pre_insert', 'hcommons_allow_society_group_admins_ham', 0 );
 
 /**
  * Make sure the_permalink() ends in /forum when posting a new topic so that
