630263754b86b66f71ed97cfa86a75ea6320757b
Ryan Williams <rwilliams@mla.org>
2018-01-23 10:26:10 -0500

    fix function name for #c3e384d

--- includes/siteorigin-panels.php
+++ includes/siteorigin-panels.php
@@ -9,7 +9,7 @@
  * Remove the 'register_widgets' action from siteorigin when on docs.
  * Otherwise, it calls url_to_postid() which creates a WP_Query on docs before the component initializes.
  */
-function remove_siteorigin_register_widgets_on_docs() {
+function hc_remove_siteorigin_register_widgets_on_docs() {
 	if ( isset( $_SERVER['REQUEST_URI'] ) && false !== strpos( $_SERVER['REQUEST_URI'], '/docs/' ) ) {
 		remove_action( 'widgets_init', array( SiteOrigin_Panels_Sidebars_Emulator::single(), 'register_widgets' ), 99 );
 	}
