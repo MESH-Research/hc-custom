0330fb7b4358421fdbf18dd711b90691ec3dacca
Nelson Alonso <nalonsopress@gmail.com>
2018-01-31 09:19:27 -0500

    After running phpcs

--- includes/buddypress-followers.php
+++ includes/buddypress-followers.php
@@ -1,13 +1,19 @@
 <?php
 /**
-   * Disables "followers" menu item on the top right
-   *
-   * @uses $wp_admin_bar
-   */
-function hcommons_admin_bar_remove_followers() {
-    global $wp_admin_bar;
+ * Customizations to buddypress followers
+ *
+ * @package Hc_Custom
+ */
 
-    $wp_admin_bar->remove_node('my-account-follow-followers');
+/**
+ * Disables "followers" menu item on the top right
+ *
+ * @uses $wp_admin_bar
+ */
+function hcommons_admin_bar_remove_followers() {
+	global $wp_admin_bar;
+
+	$wp_admin_bar->remove_node( 'my-account-follow-followers' );
 }
 
-add_action('wp_before_admin_bar_render','hcommons_admin_bar_remove_followers');
+add_action( 'wp_before_admin_bar_render','hcommons_admin_bar_remove_followers' );
