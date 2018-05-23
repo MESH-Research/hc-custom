d204b8ed3cd06a9519978d66f6d603eda0ea7d35
Nelson Alonso <nalonsopress@gmail.com>
2018-05-23 16:24:48 -0400

    Fixing some formatting

--- hc-custom.php
+++ hc-custom.php
@@ -11,8 +11,9 @@
  *
  * @package         Hc_Custom
  */
+
 if ( ! function_exists( 'is_plugin_active_for_network' ) ) {
-    require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
+	require_once( ABSPATH . '/wp-admin/includes/plugin.php' );
 }
 
 /**
