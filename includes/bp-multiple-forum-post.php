25998353eabbaae88e0ef0c9977179f4ca36ae1c
Nelson Alonso <nalonsopress@gmail.com>
2018-11-28 14:12:55 -0500

    After phpcs

--- includes/bp-multiple-forum-post.php
+++ includes/bp-multiple-forum-post.php
@@ -10,30 +10,30 @@
  *
  * @uses bpmfp_get_duplicate_topics_ids()
  * @uses bpmfp_get_this_topic_also_posted_in_message()
-**/
+ **/
 function hc_custom_add_duplicate_topics_to_forum_topic() {
 	remove_action( 'bbp_theme_after_reply_content', 'bpmfp_add_duplicate_topics_to_forum_topic' );
 
-	$reply_id = bbp_get_reply_id();
+	$reply_id   = bbp_get_reply_id();
 	$reply_post = get_post( $reply_id );
 	// Only show on the first item in a thread - the topic.
 	if ( 0 !== $reply_post->menu_order ) {
 		return;
 	}
-	$topic_id = $reply_id;
+	$topic_id      = $reply_id;
 	$all_topic_ids = bpmfp_get_duplicate_topics_ids( $topic_id );
 
 	for ( $index = 0; $index < count( $all_topic_ids ); $index++ ) {
 		// Get the forum ID for the topic, and the group ID for the forum, and the group object.
-		$topic_forum_id = get_post( $all_topic_ids[$index] )->post_parent;
+		$topic_forum_id  = get_post( $all_topic_ids[ $index ] )->post_parent;
 		$forum_group_ids = bbp_get_forum_group_ids( $topic_forum_id );
-		$forum_group_id = $forum_group_ids[0];
-		$forum_group = groups_get_group( array( 'group_id' => $forum_group_id ) );
+		$forum_group_id  = $forum_group_ids[0];
+		$forum_group     = groups_get_group( array( 'group_id' => $forum_group_id ) );
 
 		// If the group that the duplicate topic is in is public, or the current user is a member of it,
 		// or the current user is a moderator, then include a link to the topic.
-		if ( 'public' !== $forum_group->status || !groups_is_user_member( bp_loggedin_user_id(), $forum_group_id ) || !current_user_can( 'bp_moderate' ) ) {
-			unset( $all_topic_ids[$index] );
+		if ( 'public' !== $forum_group->status || ! groups_is_user_member( bp_loggedin_user_id(), $forum_group_id ) || ! current_user_can( 'bp_moderate' ) ) {
+			unset( $all_topic_ids[ $index ] );
 		}
 	}
 
