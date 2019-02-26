0168b1e30500c616f65f05fdf415923bf4c949a8
Ryan Williams <rwilliams@mla.org>
2019-02-26 12:47:46 -0500

    wording change this to your

--- includes/js/admin-mpo.js
+++ includes/js/admin-mpo.js
@@ -22,12 +22,12 @@ jQuery(function($){
    $('.option-site-visibility').children()[1].innerHTML = '<td><label class="checkbox" for="blog_public_on">' +
           '<input type="radio" id="blog_public_on" name="blog_public" value="1" checked="checked" class="styled">' +
           '<strong>Public and allow search engines to index this site. <i style="font-weight: normal">Note: it is up to search' +
-          'engines to honor your request. The site will appear in public listings around HUMANITIES COMMONS.</i></strong>' +
+          ' engines to honor your request. The site will appear in public listings around HUMANITIES COMMONS.</i></strong>' +
        '</label><br/>' +
        '<label class="checkbox" for="blog_public_off">' +
           '<input type="radio" id="blog_public_off" name="blog_public" value="0" class="styled">' +
           '<strong>Public but discourage search engines from index this site. <i style="font-weight: normal">Note: this option' +
-          'does not block access to your site — it is up to search engines to honor your request. The site will appear in' +
+          ' does not block access to your site — it is up to search engines to honor your request. The site will appear in' +
           'public listings around HUMANITIES COMMONS</i></strong>' +
        '</label><br/>' +
        '<label class="checkbox" for="blog-private-1"><input id="blog-private-1" type="radio" name="blog_public" value="-1" class="styled"><strong>Visible only to registered users of '+ society() +'</strong></label>' +
