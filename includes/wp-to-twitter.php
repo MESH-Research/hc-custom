935b9aafa3463ce396c7a0f415413f6e675bb329
Mike Thicke <thickemi@msu.edu>
2023-02-01 19:12:33 +0000

    commons-143-remove-ajs-caa

--- includes/wp-to-twitter.php
+++ includes/wp-to-twitter.php
@@ -17,14 +17,6 @@ add_action( 'dynamic_sidebar_after', 'hc_custom_dequeue_twitter_platform' );
 
 function hc_custom_society_to_twitter( $society_id ) {
     switch ($society_id) {
-    	case "caa":
-    		return "https://twitter.com/caavisual";
-        	break;
-    	
-    	case "ajs":
-    		return "https://twitter.com/jewish_studies";
-        	break;
- 
         case "up":
  		return "https://twitter.com/aupresses";
 		break;
